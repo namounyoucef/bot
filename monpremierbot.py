a29d89e9e6fc10f7572eaf48d412453a138a29f5
namounyoucef <namoun.youcef.1@gmail.com>
2020-02-06 14:23:04 +0100

    bot 0.3

--- monpremierbot.py
+++ monpremierbot.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-#salutation = ['Hello', 'Ciao', 'Goodbye', 'Salam', 'Hy', 'GoodNight', 'Holla', 'BonesNuches', 'SeeYou', 'GoodAfternoon']
+import random
+import re
+salutation = ['Hello', 'Ciao', 'Goodbye', 'Salam', 'Hy', 'GoodNight', 'Holla', 'BonesNuches', 'SeeYou', 'GoodAfternoon']
 
-question = input("Bonjour, je suis votre premier bot, veillez inseré un mot\n")
-
+reponse = input("Bonjour, je suis votre premier bot, veillez inseré un mot!\n")
 proposition = ["salut", "bonjour", "bonsoir"]
-if (question.lower() in proposition):
-    print("Salut mon ami")
+if (reponse.lower() in proposition):
+
+    print(random.choice(salutation) + " mon ami")
 else:
     print("je ne comprend rien")
