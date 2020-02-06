65f314a46db61d71b47911308f71b12627fabc1f
namounyoucef <namoun.youcef.1@gmail.com>
2020-02-06 16:06:40 +0100

    bot 0.3

--- monpremierbot.py
+++ monpremierbot.py
@@ -1,12 +1,29 @@
 #!/usr/bin/python3
 import random
 import re
-salutation = ['Hello', 'Ciao', 'Goodbye', 'Salam', 'Hy', 'GoodNight', 'Holla', 'BonesNuches', 'SeeYou', 'GoodAfternoon']
+import datetime
+#salutation = ['Hello', 'Ciao', 'Goodbye', 'Salam', 'Hy', 'GoodNight', 'Holla', 'BonesNuches', 'SeeYou', 'GoodAfternoon']
+categories = ["voiture", "telephone", "ordinateur"]
+exclure = ["je", "tu", "bien", "nous", "svp", "bonjour"]
+#question = input("Bonjour, je suis votre premier bot, veillez inseré un mot!\n")
+requete = input ("bonjour, je suis votre assistant MyBot, Que desirez vous achetez ? \n")
+decoupe = (re.findall(r"[a-zA-Z]+", requete))
+#print (decoupe)
+final = list(set(decoupe) - set(exclure))
+finale = [mot.lower() for mot in final if len(mot) > 2 ]
+#print(finale)
 
-reponse = input("Bonjour, je suis votre premier bot, veillez inseré un mot!\n")
-proposition = ["salut", "bonjour", "bonsoir"]
-if (reponse.lower() in proposition):
-
-    print(random.choice(salutation) + " mon ami")
+#proposition = ["salut", "bonjour", "bonsoir"]
+if "voiture" in finale : 
+    print("voici le catalogue de voitures")
+elif "telephone" in finale :
+    print("voici le catalogue de telephone")
+elif "ordinateur" in finale :
+    print("voici notre game d'ordinateur ")
 else:
-    print("je ne comprend rien")
+    print("vu a {0}".format(datetime.datetime.now())) 
+#if (reponse.lower() in proposition):
+
+   # print(random.choice(salutation) + " mon ami")
+#else:
+   # print("je ne comprend rien")
