914eefa24221c4b8b6947071cb059dcdbdd535fe
namounyoucef <namoun.youcef.1@gmail.com>
2020-02-06 11:05:06 +0100

    mon premier bot

---
+++ monpremierbot.py
@@ -0,0 +1,11 @@
+#!usr/bin/python3
+#salutation = ['Hello', 'Ciao', 'Goodbye', 'Salam', 'Hy', 'GoodNight', 'Holla', 'BonesNuches', 'SeeYou', 'GoodAfternoon']
+
+question = input("Bonjour, je suis votre premier bot, veillez inseré un mot")
+if (question.lower() == "salut"):
+    print("Salut mon ami")
+elif (question.lower() == "bonjour"):
+    print("Salut mon ami")
+elif (question.lower() == "bonsoir"):
+    print ("salut mon ami")
+elif ("je ne comprend rien")
